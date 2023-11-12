859cc833203b2acc31584b1a99f2bc7a3a5aa204
Christinne <vanessa0christinne@gmail.com>
2023-11-13 00:10:19 +0100

    Add style

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from .views import get_country_info, home_view
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('country-info/', get_country_info, name='country_info'),
     path('', home_view, name='home'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
